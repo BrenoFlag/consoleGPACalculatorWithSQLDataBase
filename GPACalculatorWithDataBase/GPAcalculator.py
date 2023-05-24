231410ea7d466589e36ee033eab19d4c34bfaa93
BrenoFlag <117334417+BrenoFlag@users.noreply.github.com>
2023-05-23 22:47:00 -0700

    Add files via upload

--- GPACalculatorWithDataBase/GPAcalculator.py
+++ GPACalculatorWithDataBase/GPAcalculator.py
@@ -3,92 +3,7 @@ from MySQLGPAcalculatorDB import GPACalculatorDatabase
 connection = GPACalculatorDatabase()
 
 
-def seeClassesDB():
-    classes = connection.getTable('classes')
-    for element in classes:
-        print(f'{element[0]}) {element[1]}')
-    print("\n")
-
-
 class GPACalculator:
-
-    def seeClasses(self):
-        """ Allows user to visualize existing data and modify it if desired """
-        classes = self.ClassesContent.splitlines()
-
-        # Prints a menu with all classes
-        x = 1
-        for i in classes:
-            print(f"{x}) {i}")
-            x += 1
-
-        while True:
-            try:
-                determiner = int(input("Select a class number to see details/modify\n"
-                                       "\'0\' to return to main menu\n"))
-                if 0 <= determiner <= len(classes):
-                    break
-                else:
-                    print("Enter a valid number!")
-            except ValueError:
-                print("Input must be a valid number!")
-
-        if determiner != 0:
-            grades = self.ClassGradeContent.splitlines()
-            creditHour = self.CreditAmountContent.splitlines()
-
-            print(f"1) Class name: {classes[determiner - 1]}\n"
-                  f"2) Credit amount: {creditHour[determiner - 1]}\n"
-                  f"3) Grade: {grades[determiner - 1]}\n")
-
-            while True:
-                # prints details of selected class.
-                try:
-                    selector = int(input("What would you like to modify?\n"
-                                         "Select \'0\' to go back to main menu\n"))
-                    if 0 <= selector < 4:
-                        break
-                    else:
-                        print("Enter a valid number")
-                except ValueError:
-                    print("Enter a valid number!")
-
-            if selector == 1:
-                classes[determiner - 1] = input("Enter new class name: ")
-
-                content = ""
-                for elements in classes:
-                    content += f"{elements}\n"
-
-                self.toClasses.write_text(content)
-
-            elif selector == 2:
-                while True:
-                    try:
-                        creditHour[determiner - 1] = int(input("Enter new credit amount: "))
-                        break
-                    except ValueError:
-                        print("Input must be a number")
-
-                content = ""
-                for elements in creditHour:
-                    content += f"{elements}\n"
-
-                self.toCreditAmount.write_text(content)
-
-            elif selector == 3:
-                while True:
-                    try:
-                        grades[determiner - 1] = int(input("Enter new grade: "))
-                        break
-                    except ValueError:
-                        print("Input must be a number!")
-
-                content = ""
-                for elements in grades:
-                    content += f"{elements}\n"
-
-                self.toClassGrade.write_text(content)
 
     @staticmethod
     def getMenu():
@@ -97,7 +12,8 @@ class GPACalculator:
               "3) See classes\n"
               "4) Quit\n")
 
-    def getGPA(self):
+    @staticmethod
+    def getGPA():
         grades = []
         creditHours = []
 
@@ -123,7 +39,8 @@ class GPACalculator:
         except ZeroDivisionError:
             print("GPA: Undefined")
 
-    def addClass(self):  # updated to database
+    @staticmethod
+    def addClass():  # updated to database
         connection.appendToDatabase(input("Enter the name of the class: "),
                                     int(input("Enter the Grade: ")),
                                     int(input("Enter the credit amount: ")))
